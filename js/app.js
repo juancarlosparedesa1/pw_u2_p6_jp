eab43f4b5a9eaa88436563b810d0cad4a5592771
cdto017 <cdanielcdto1@gmail.com>
2024-05-29 19:32:58 -0500

    vue directivas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
 console.log(Vue);
 
+const estudiantes=[{nombre:"Edison",apellido:"Cayambe"},
+{nombre:"Carlos",apellido:"Paez"},
+{nombre:"Daniel",apellido:"Castillo"},
+{nombre:"Andrea",apellido:"Mora"},
+{nombre:"Andres",apellido:"Teran"}];
+
+
 const app=Vue.createApp({//backtips alt+96
-    template:`
+   /*  template:`
     <h1>Hola Mundo desde Vue.JS</h1>
     <p>{{1+1}}</p>
     <p>{{[1,2,3,4]}}</p>
     <p>{{false?'True':'False'}}</p>
-    `
+    ` */
+    /* opción de optin api */
+    /* nomenclatura para dclarara una propiedad reactiva */
+    data(){
+        return{
+            mensaje:"hola Mundo Propiedad Reactiva",
+            valor:7,
+            lista:estudiantes
+        }
+    },
+    methods:{
+        cambiarTexto(){
+            this.mensaje="TextoCambiado";
+        },
+        sumar(){
+            this.valor=this.valor+100;
+        },
+        agregar(){
+            const nuevo={
+                nombre:"Juan Carlos",
+                apellido:"Paredes Angulo "
+            }
+            this.lista.unshift(nuevo);
+        }
+    }
 });
 
 app.mount('#myApp');//puede ser cualquier nombre pero lo normal es esto
